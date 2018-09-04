5af5c3aaba7e2270610daa45a1eaffb6ac27a064
Kalman Hazins <kalmanh@gmail.com>
2018-09-03 23:21:06 -0400

    Add more description; restrict limit

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,10 +5,10 @@ class CountriesController < ApplicationController
     # If limit is passed in - do nothing
     # Otherwise, generate a random number between 5 and 25
 
-    @limit = params[:limit].to_i
-    @limit = rand(5..25) if @limit.zero?
+    limit = params[:limit].to_i
+    limit = rand(5..25) if limit.zero?
 
-    @countries_to_display = all_countries.sample(@limit)
+    @countries_to_display = all_countries.sample(limit)
     @countries_to_display.sort! { |a, b|  a["name"] <=> b["name"] }
   end
 end
